d64468b45edb1d79677a6be427ca1479f42d00fd
krishnapanjiyar <krishna.panjiyar789@gmail.com>
2022-04-02 12:29:05 -0700

    Creating sockets in the server and binding it

--- server.py
+++ server.py
@@ -1,6 +1,38 @@
+from re import S
 import socket
 import sys
 
-# create a socket ( connect two computers)
+# Create a Socket ( connect two computers)
 def create_socket():
-    
+    try:
+        global host
+        global port
+        global s
+        host = ""
+        port = 9999
+        s = socket.socket()
+
+    except socket.error as msg:
+        print("socket creation error: " + str(msg))
+
+# Binding the socket and listening for connections
+def bind_socket():
+    try:
+        global host
+        global port
+        global s
+
+        print("Binding the Port" + str(port))
+
+        s.bind((host, port))
+        s.listen(5)
+
+    except socket.error as msg:
+        print("Socket Binding error" + str(msg) + "\n" + "Retrying...")
+        bind_socket()
+
+# Establish connection with a client (socket must be listening)
+
+def socket_accept():
+    conn.address = s.accept()
+    print()
